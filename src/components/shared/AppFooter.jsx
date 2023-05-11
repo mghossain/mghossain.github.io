44b1a2e7f593820bae1e47d42a9c8213a93abff6
mike <mghossain.42@gmail.com>
2023-05-11 22:44:14 +0300

    update footer links

--- src/components/shared/AppFooter.jsx
+++ src/components/shared/AppFooter.jsx
@@ -11,28 +11,28 @@ const socialLinks = [
 	{
 		id: 1,
 		icon: <FiGlobe />,
-		url: 'https://www.stoman.me/',
+		url: 'https://mghossain.github.io/',
 	},
 	{
 		id: 2,
 		icon: <FiGithub />,
-		url: 'https://github.com/realstoman',
+		url: 'https://github.com/mghossain',
 	},
-	{
-		id: 3,
-		icon: <FiTwitter />,
-		url: 'https://twitter.com/realstoman',
-	},
+	// {
+	// 	id: 3,
+	// 	icon: <FiTwitter />,
+	// 	url: 'https://twitter.com/realstoman',
+	// },
 	{
 		id: 4,
 		icon: <FiLinkedin />,
-		url: 'https://www.linkedin.com/in/realstoman',
+		url: 'https://www.linkedin.com/in/michael-ghossain-lb/',
 	},
-	{
-		id: 5,
-		icon: <FiYoutube />,
-		url: 'https://www.youtube.com/c/realstoman',
-	},
+	// {
+	// 	id: 5,
+	// 	icon: <FiYoutube />,
+	// 	url: 'https://www.youtube.com/c/realstoman',
+	// },
 ];
 
 const AppFooter = () => {
