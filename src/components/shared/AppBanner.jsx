4a530336479e8d980556033445f336473841fa1b
mike <mghossain.42@gmail.com>
2023-05-11 19:39:58 +0300

    add logo, edit AppBanner.jsx

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -14,7 +14,7 @@ const AppBanner = () => {
 			transition={{ ease: 'easeInOut', duration: 0.9, delay: 0.2 }}
 			className="flex flex-col sm:justify-between items-center sm:flex-row mt-12 md:mt-2"
 		>
-			<div className="w-full md:w-1/3 text-left">
+			<div className="w-full md:w-1/3 text-center">
 				<motion.h1
 					initial={{ opacity: 0 }}
 					animate={{ opacity: 1 }}
@@ -25,7 +25,9 @@ const AppBanner = () => {
 					}}
 					className="font-general-semibold text-2xl lg:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase"
 				>
-					Hi, Iam Stoman
+					<div className='text-center'>
+						Hi, I'm Michael
+					</div>
 				</motion.h1>
 				<motion.p
 					initial={{ opacity: 0 }}
@@ -35,9 +37,11 @@ const AppBanner = () => {
 						duration: 0.9,
 						delay: 0.2,
 					}}
-					className="font-general-medium mt-4 text-lg md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
+					className="font-general-medium mt-4 md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
 				>
-					A Full-Stack Developer & Design Enthusiast
+					<div className='text-center'>
+						A Full-Stack Developer <br/> Laravel | PHP <br/> React | JavaScript
+					</div>
 				</motion.p>
 				<motion.div
 					initial={{ opacity: 0 }}
@@ -50,15 +54,16 @@ const AppBanner = () => {
 					className="flex justify-center sm:block"
 				>
 					<a
-						download="Stoman-Resume.pdf"
-						href="/files/Stoman-Resume.pdf"
-						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
+						download="Michael Ghossain - Web Developer CV 2023.pdf"
+						href="/files/Michael Ghossain - Web Developer CV 2023.pdf"
+						className="font-general-medium flex justify-center mx-auto items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
 						aria-label="Download Resume"
 					>
 						<FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
 						<span className="text-sm sm:text-lg font-general-medium duration-100">
 							Download CV
 						</span>
+
 					</a>
 				</motion.div>
 			</div>
