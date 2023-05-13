055d685ef0690a32fddc09c0c62935dcf44fb691
mike <mghossain.42@gmail.com>
2023-05-13 03:42:38 +0300

    add download CV button in AboutMeBio.jsx

--- src/components/about/AboutMeBio.jsx
+++ src/components/about/AboutMeBio.jsx
@@ -1,6 +1,7 @@
 import profileImage from '../../images/profile.jpeg';
 import { useContext } from 'react';
 import AboutMeContext from '../../context/AboutMeContext';
+import {FiArrowDownCircle} from "react-icons/fi";
 
 const AboutMeBio = () => {
 	const { aboutMe } = useContext(AboutMeContext);
@@ -20,7 +21,20 @@ const AboutMeBio = () => {
 						{bio.bio}
 					</p>
 				))}
+				<a
+					download="Michael Ghossain - Web Developer CV 2023.pdf"
+					href="/files/Michael Ghossain - Web Developer CV 2023.pdf"
+					className="font-general-medium  flex justify-center mx-auto items-center w-36 sm:w-48 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
+					aria-label="Download Resume"
+				>
+					<FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
+					<span className="text-sm sm:text-lg font-general-medium duration-100">
+						Download CV
+					</span>
+
+				</a>
 			</div>
+
 		</div>
 	);
 };
