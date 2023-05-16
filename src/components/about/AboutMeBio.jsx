1061c238deaefdf07c13b5bda513aff91cd7d0d3
mike <mghossain.42@gmail.com>
2023-05-16 09:06:33 +0300

    add tech stack and skills in about me

--- src/components/about/AboutMeBio.jsx
+++ src/components/about/AboutMeBio.jsx
@@ -6,6 +6,13 @@ import {FiArrowDownCircle} from "react-icons/fi";
 const AboutMeBio = () => {
 	const { aboutMe } = useContext(AboutMeContext);
 
+	function bioStyling(bio) {
+		if (bio === 'Tools & Technologies')
+			return 'font-bold text-center mt-5'
+		if (bio === 'Tech Stack:' || bio === 'Agile Skills:')
+			return 'font-semibold'
+	}
+
 	return (
 		<div className="block sm:flex sm:gap-10 mt-10 sm:mt-20">
 			<div className="w-full sm:w-1/4 mb-7 sm:mb-0">
@@ -15,7 +22,7 @@ const AboutMeBio = () => {
 			<div className="font-general-regular w-full sm:w-3/4 text-left">
 				{aboutMe.map((bio) => (
 					<p
-						className="mb-4 text-ternary-dark dark:text-ternary-light text-lg"
+						className={ bioStyling(bio.bio) + ' mb-4 text-ternary-dark dark:text-ternary-light text-lg' }
 						key={bio.id}
 					>
 						{bio.bio}
@@ -24,7 +31,7 @@ const AboutMeBio = () => {
 				<a
 					download="Michael Ghossain - Web Developer CV 2023.pdf"
 					href="/files/Michael Ghossain - Web Developer CV 2023.pdf"
-					className="font-general-medium  flex justify-center mx-auto items-center w-36 sm:w-48 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
+					className="font-general-medium  flex justify-center mx-auto mt-12 items-center w-36 sm:w-48 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
 					aria-label="Download Resume"
 				>
 					<FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -2,9 +2,23 @@ export const aboutMeData = [
 	{
 		id: 1,
 		bio: 'As a full-stack web application developer with expertise in Laravel, PHP, React, and JavaScript, I am committed to designing and creating high-quality projects that exceed client expectations. Through my work as a self-motivated freelancer, I have demonstrated my ability to deliver custom web applications, services, and tools to businesses across diverse industries.',
-	},
-	{
+	}, {
 		id: 2,
 		bio: 'I am passionate about developing my expertise in new workplaces, with a particular focus on programming, web development, and management. I am highly adaptable and flexible, able to quickly adjust to changing environments and deliver results in diverse situations.',
+	}, {
+		id: 3,
+		bio: 'Tools & Technologies'
+	},{
+		id: 4,
+		bio: 'Tech Stack:'
+	},{
+		id: 5,
+		bio: 'PHP, Laravel, Blade, JavaScript, CSS, Html, jQuery, React.js, Tailwind CSS, Bootstrap, Data tables, Select2, Git, GitLab, GitHub, Jira, SQL, SQLite, Elasticsearch, Kibana, Postman, Ajax, Web Servers (Apache, tomcat), Python, Java, Docker, C#, C++, .NET.'
+	},{
+		id: 6,
+		bio: 'Agile Skills:'
+	},{
+		id: 6,
+		bio: 'Restful APIs, TDD, Repository design, Refactoring, Client/Server, Microservices architecture.'
 	},
 ];
